39433bc756f75607168185d8f498e71a016f34be
DSFYS <peng820401@outlook.com>
2018-11-14 18:36:43 +0800

    其他修改

---
+++ practice/024-sass/nested-rulse/yo.js
@@ -0,0 +1,86 @@
+/*console.log("yo wrold");
+(function () {
+    'use strict';
+})();
+
+function aaa() {
+    'use strict'
+}
+
+;
+(function () {
+    'use strict'
+})();
+
+function AA() {
+    "use strict"
+}
+
+function aaaa() {
+    "use strict"
+};
+
+function bbb() {
+    "use strict"
+}
+
+(function () {
+    "use strict"
+})();
+(function () {
+    "use strict"
+})();
+
+; (function () {
+    "use strict"
+})();
+; (function aAAd() {
+    "use strict"
+})();
+let a = 1;
+let bn = 2;
+console.log(a);
+console.log(bn);
+console.log(a + bn);
+console.log(a + bn);
+console.log(a + bn);
+
+
+const A = "11";
+const PI = 3.14159246
+const NAME = "zzz"
+const shrn = "23"
+console.log(a + shrn);
+
+
+console.log("aaa")
+const const 
+const const const const const const console console console console 
+console consolr
+consoleconsole console console console  console const ; (function () {
+    "use strict"
+})();
+const console.console.log();
+function aaaaaaaa() { 
+    "use strict"
+}
+const ;
+console.log("zhenshi ");*/
+// let name ="whh"
+// let nickname =name;
+// console.log(name);
+// console.log(nickname);
+let name, nickname;
+name = "whh";
+nickname = name;
+console.log(name);
+console.log(nickname);
+let a, b, c;
+a = 1;
+b = 2;
+c = a + b;
+console.log(a);
+console.log(b);
+console.log(c);
+const A = 1;
+console.log(a, b, c);
---
+++ practice/025-DataType/main.js
@@ -0,0 +1,57 @@
+// let a = "aaa";
+// let b = 14;
+// let none = null;
+// let nane = undefined;
+// // undefined
+// // undefined
+// // undefined
+// // undefined
+// let listdata = [
+//     1, 2, 3, 4, 5
+// ];
+// let datatype = {
+//     name:"cccc",
+//     age:15
+// }
+// var arr = [{
+//     'key1': 'value1',
+//     'key2': 'value2',
+//     'key3': 'value3'
+// }, {
+//     'key1': 'value4',
+//     'key2': 'value5',
+//     'key3': 'value6'
+// }, {
+//     'key1': 'value7',
+//     'key2': 'value8',
+//     'key3': 'value9'
+//     }];
+
+// let obj = {
+//     a: [3, 5],
+//     b: ['Yo', 1],
+//   };
+
+// let obj = {
+//   a: {foo: 2},
+//   yo: [3, 5],
+//   b: {
+//     la: {
+//       name: 'Wood',
+//       x: [5, 'a', 'c'],
+//       muhaha: ['Bob', 'Huahua'],
+//       length: 100,
+//     },
+//   },
+// };
+
+let obj = {
+  la: 'Ha',
+  v: 2,
+  p: [2, 5],
+  b: {a: {name: 'White',
+      yo: [5, 'a', 'c'],
+      length: 100,
+      map: true,},
+  },
+};
